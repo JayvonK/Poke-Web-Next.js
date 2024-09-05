0d8c528de95fabb1ace744d95b344c28be79adcd
JayvonnK <jaykhuth@gmail.com>
2024-09-04 20:16:33 -0700

    editing interface for pokemon data

--- src/interfaces/PokeInterfaces.ts
+++ src/interfaces/PokeInterfaces.ts
@@ -7,8 +7,46 @@ interface PokeAbilities {
   slot: number;
 }
 
-export interface PokeData {
-  abilities: PokeAbilities[];
-  
+interface PokeMoves {
+  move: {
+    name: string;
+    url: string;
+  }
 }
 
+interface PokeType {
+  type: {
+    name: string;
+    url: string;
+  }
+}
+
+interface PokeSprites {
+  other: {
+    ["official-artwork"]: {
+      front_default: string;
+      front_shiny: string;
+    }
+  },
+  versions: {
+    ["generation-v"]: {
+      ["black-white"]: {
+        animated: {
+          front_default: string;
+        }
+      }
+    }
+  }
+}
+
+export interface PokeData {
+  abilities: PokeAbilities[];
+  moves: PokeMoves[];
+  sprites: PokeSprites;
+  species: {
+    name: string;
+    url: string;
+  }
+  types: PokeType[];
+  weight: number;
+}
