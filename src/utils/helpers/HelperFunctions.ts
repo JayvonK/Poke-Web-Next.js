2df53113c4e7e5d28eefce293e3a86b4722b2159
JayvonK <jaykhuth@gmail.com>
2024-09-26 09:06:31 -0700

    editing more of the helper function

--- src/utils/helpers/HelperFunctions.ts
+++ src/utils/helpers/HelperFunctions.ts
@@ -1,18 +1,16 @@
 import { PokeMoves } from "@/interfaces/PokeData";
 
 export const capatilizeFirstLetter = (word: string | PokeMoves[]) => {
-  // Grabbing first letter with charAt and making it uppercase, then concatenating the rest of the string using .slice to grab all the letters starting at the index of 1
-
-  if (typeof word === 'string') {
-    return word.charAt(0).toUpperCase() + word.slice(1);
-  } else {
-    let emptyString = "";
-    word.forEach(n => {
-      let name = n.move.name;
-      let cap = name.charAt(0).toUpperCase() + name.slice(1) + ', ';
-      emptyString += cap;
-    });
-    return emptyString;
+  if (typeof word === "string") {
+    return word[0].toUpperCase() + word.slice(1);
   }
 
-}
+  return word
+    .map((m) =>
+      m.move.name
+        .split("-")
+        .map((p) => p[0].toUpperCase() + p.slice(1))
+        .join(" ")
+    )
+    .join(", ");
+};
